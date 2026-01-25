a3a4882067f15435bac55f760c0ad55680526009
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2026-01-25 14:33:07 -0300

    feat: serviço de personagens consumo de dados da API de personagens de Avatar

---
+++ app-api/src/app/services/personagens-service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { IPersonagens } from '../models/ipersonagens';
+import { Observable
+
+ } from 'rxjs';
+@Injectable({
+  providedIn: 'root',
+})
+export class PersonagensService {
+    constructor(private http:HttpClient){}
+    
+    listarPersonagens(): Observable <Array<IPersonagens>>{
+      return this.http.get<Array<IPersonagens>>("https://last-airbender-api.fly.dev/api/v1/characters")
+    }
+
+    detalharPersonagemId(id: string):Observable<IPersonagens>{
+      return this.http.get<IPersonagens>(`https://last-airbender-api.fly.dev/api/v1/characters/${id}`)
+    }
+}
