4307d46b0eeb250d04e2ccd4ef36ba68ef1f9c38
dev-poliana-escolar <poliana.pinheiro@escolar.ifrn.edu.br>
2026-01-25 14:40:27 -0300

    feat: rotas e configuração para fazer requests http

--- app-api/src/app/app.routes.ts
+++ app-api/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', loadComponent: () => import('./personagens/personagens').then(m => m.Personagens) },
+    { path: 'personagem-detalhe/:id', loadComponent: () => import('./personagem-detalhe/personagem-detalhe').then(m => m.PersonagemDetalhe) }
+];
